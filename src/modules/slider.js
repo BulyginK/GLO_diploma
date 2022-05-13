ee02f6b6867511a6128613f1cc8db0cb79ac8d4e
BulyginK <bulygin.k@gmail.com>
2022-05-13 21:49:44 +0300

    benefitsSlider

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,36 +1,63 @@
 const slider = () => {
-    const benefitsItems = document.querySelectorAll('.benefits__item');
+    const benefits = document.querySelector('.benefits-wrap');
     const benefitsArrows = document.querySelector('.benefits-arrows');
+    let benefitsItems = document.querySelectorAll('.benefits__item');
     let benefitsArr = [];
 
-    for (let i = 0; i < benefitsItems.length; i++ ) {
-        benefitsArr.push(benefitsItems[i])
+    const newArr = () => {
+        benefitsArr = [];
+        for (let i = 0; i < benefitsItems.length; i++) {
+            benefitsItems[i].style.display = 'none';
+            benefitsArr.push(benefitsItems[i]);
+        }
+    }
+
+    const prevSlide = () => {
+        let lastElem = benefitsItems[benefitsArr.length - 1];
+        lastElem.remove(); //удаляется последний элемент
+        benefitsItems[0].before(lastElem); // он добавляется в начало блока
+        benefitsItems = document.querySelectorAll('.benefits__item'); // переопределяется объект слайдов
+        newArr(); //пересобирается массив слайдов
     }
 
     const nextSlide = () => {
-        for (let i = 0; i < benefitsItems.length; i++ ) {
-            if(i < 3) {
-                benefitsArr[i].style.display = 'block';
-            } else {
-                benefitsArr[i].style.display = 'none';
+        let firstElem = benefitsArr[0];
+        firstElem.remove(); //удаляется первый элемент
+        benefitsItems[benefitsArr.length - 1].after(firstElem); // он добавляется в конец блока
+        benefitsItems = document.querySelectorAll('.benefits__item'); // переопределяется объект слайдов
+        newArr(); //пересобирается массив слайдов
+    }
+
+    const showSlide = () => {
+        if (window.screen.width >= 576) {
+            for (let i = 0; i < benefitsItems.length; i++) {
+                if (i < 3) {
+                    benefitsArr[i].style.display = 'block';
+                }
+            }
+        } else {
+            for (let i = 0; i < benefitsItems.length; i++) {
+                if (i == 0) {
+                    benefits.style.justifyContent = 'center';
+                    benefitsArr[i].style.display = 'block';
+                }
             }
         }
     }
-    
+
     benefitsArrows.addEventListener('click', (e) => {
         e.preventDefault();
         if (e.target.matches('#benefits__arrow--left')) {
-
-        } else  if (e.target.matches('#benefits__arrow--right')) {
-            let firstElem = benefitsArr[0];
-            benefitsArr.shift();
-            benefitsArr.push(firstElem);
-            console.log(benefitsArr);
-            nextSlide()
+            prevSlide();
+            showSlide();
+        } else if (e.target.matches('#benefits__arrow--right')) {
+            nextSlide();
+            showSlide();
         }
     })
 
-    nextSlide()
+    newArr();
+    showSlide();
 }
 
 export default slider
