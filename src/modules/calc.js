684bbe99fc0ca8fed978da50350eb474d3404ef8
BulyginK <bulygin.k@gmail.com>
2022-05-18 22:20:28 +0300

    calc

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,30 +1,32 @@
 const calc = () => {
-    const calc = document.querySelector('#calc');
-    const calcType = document.querySelector('#calc-type');
-    const calcTypeMaterial = document.querySelector('#calc-type-material');
-    const calcInput = document.querySelector('#calc-input');
-    const calcTotal = document.querySelector('#calc-total');
+    if (document.querySelector('#calc')) {
+        const calc = document.querySelector('#calc');
+        const calcType = document.querySelector('#calc-type');
+        const calcTypeMaterial = document.querySelector('#calc-type-material');
+        const calcInput = document.querySelector('#calc-input');
+        const calcTotal = document.querySelector('#calc-total');
 
-    const countCalc = () => {
-        calcTotal.disabled = false;
-        let valueTypeMaterial = 1;
+        const countCalc = () => {
+            calcTotal.disabled = false;
+            let valueTypeMaterial = 1;
 
-        if (calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value > 0) {
-            valueTypeMaterial = calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value
+            if (calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value > 0) {
+                valueTypeMaterial = calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value
+            }
+
+            calcTotal.value = Math.round(calcType.options[calcType.selectedIndex].value * calcInput.value * valueTypeMaterial);
         }
 
-        calcTotal.value = Math.round(calcType.options[calcType.selectedIndex].value * calcInput.value * valueTypeMaterial);
+        calc.addEventListener('input', () => {
+            calcInput.value = calcInput.value.replace(/\D/, "");
+            if (calcType.selectedIndex != 0 && calcInput.value > 0) {
+                countCalc();
+            }
+            else if (calcType.selectedIndex == 0 || calcInput.value == 0 || calcTypeMaterial.selectedIndex == 0) {
+                calcTotal.value = '';
+            }
+        })
     }
-
-    calc.addEventListener('input', () => {
-        calcInput.value = calcInput.value.replace(/\D/, "");
-        if (calcType.selectedIndex != 0 && calcInput.value > 0) {
-            countCalc();
-        }
-        else if (calcType.selectedIndex == 0 || calcInput.value == 0 || calcTypeMaterial.selectedIndex == 0) {
-            calcTotal.value = '';
-        }
-    })
 }
 
 export default calc
