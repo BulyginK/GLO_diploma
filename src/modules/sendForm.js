eabc5df52bbf77613623b23c05906034cd6aab97
BulyginK <bulygin.k@gmail.com>
2022-05-20 00:20:23 +0300

    правка форм

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,115 +1,144 @@
 const sendForm = () => {
-    const forms = document.querySelectorAll('.order-form>form');
-    
+    const forms = document.querySelectorAll('form');
+
     forms.forEach((elem) => {
         const nameForm = elem.name;
-        sendForms(nameForm);
+        sendForms({
+            formName: nameForm,
+            someElem: [
+                {
+                    id: 'calc-total'
+                }
+            ]
+        })
     })
 }
 
-const sendForms = (nameForm) => {
-    const form = document.querySelector('[name="' + nameForm + '"]');
-    const btn = form.querySelector('button');
-    const inputName = form.querySelectorAll('[name="fio"]');
-    const inputPhone = form.querySelectorAll('[name="phone"]');
+const sendForms = ({ formName, someElem = [] }) => {
+    const form = document.querySelector('[name="' + formName + '"]');
 
-    const textOriginal = btn.textContent;
-    const loadText = 'Отправка...';
-    const errorText = 'Ошибка отправки!';
-    const successText = 'Скидка Ваша!';
 
-    const regularName = /^[а-яА-ЯЁёA-Za-z]+$/;
-    const regularPhone = /^\+[\d]{0,16}$/;
+    // const btn = form.querySelector('button');
+    // const inputName = form.querySelectorAll('[name="fio"]');
+    // const inputPhone = form.querySelectorAll('[name="phone"]');
 
-    const validate = () => {
-        let success = true;
-        const userName = form.querySelector('[name="fio"]');
-        const userPhone = form.querySelector('[name="phone"]');
+    // const textOriginal = btn.textContent;
+    // const loadText = 'Отправка...';
+    // const errorText = 'Ошибка отправки!';
+    // const successText = 'Скидка Ваша!';
 
-        if (regularName.test(userName.value) && regularPhone.test(userPhone.value)) {
-            return success
-        } else {
-            return success = false
-        }
-    }
+    // const regularName = /^[а-яА-ЯЁёA-Za-z]+$/;
+    // const regularPhone = /^\+[\d]{0,16}$/;
 
-    const sendData = (data) => {
-        return fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(res => res.json());
-    }
+    // const validate = () => {
+    //     let success = true;
+    //     const userName = form.querySelector('[name="fio"]');
+    //     const userPhone = form.querySelector('[name="phone"]');
 
-    const submitForm = () => {
-        const formElements = form.querySelectorAll('input');
-        const formData = new FormData(form);
-        const formBody = {};
+    //     if (regularName.test(userName.value) && regularPhone.test(userPhone.value)) {
+    //         return success
+    //     } else {
+    //         return success = false
+    //     }
+    // }
 
-        btn.textContent = loadText;
+    // const sendData = (data) => {
+    //     return fetch('https://jsonplaceholder.typicode.com/posts', {
+    //         method: 'POST',
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         }
+    //     }).then(res => res.json());
+    // }
 
-        formData.forEach((val, key) => {
-            formBody[key] = val;
-        })
+    // const submitForm = () => {
+    //     const formElements = form.querySelectorAll('input');
+    //     const formData = new FormData(form);
+    //     const formBody = {};
 
-        if (validate()) {
-            sendData(formBody)
-                .then(data => {
-                    btn.textContent = successText;
+    //     btn.textContent = loadText;
 
-                    formElements.forEach(input => {
-                        input.value = '';
-                    })
-                })
-                .catch(error => {
-                    btn.textContent = errorText;
-                })
-        } else {
-            btn.textContent = errorText;
-            formElements.forEach(input => {
-                input.value = '';
-            })
-        }
-    }
+    //     formData.forEach((val, key) => {
+    //         formBody[key] = val;
+    //     })
 
-    const inputError = (inputElem, regular, alert) => {
-        for (let i = 0; i < inputElem.length; i++) {
-            const textError = document.createElement('span');
+    //     someElem.forEach(elem => {
+    //         const element = document.getElementById(elem.id);
 
-            inputElem[i].parentElement.append(textError);
-            textError.classList.add('help-block');
+    //         if (elem.type === 'block') {
+    //             formBody[elem.id] = element.textContent;
+    //         } else if (elem.type === 'input') {
+    //             formBody[elem.id] = element.value;
+    //         }
+    //     })
 
-            inputElem[i].addEventListener('input', (e) => {
-                textError.innerHTML = '';
-                e.target.classList.remove('error');
-                if (!regular.test(e.target.value)) {
-                    e.target.classList.add('error');
-                    textError.innerHTML = alert;
-                }
-            });
-        };
-    }
+    //     if (validate()) {
+    //         sendData(formBody)
+    //             .then(data => {
+    //                 btn.textContent = successText;
 
-    try {
-        if (!form) {
-            throw new Error('Верните форму на место, пожааалуйста))')
-        }
+    //                 formElements.forEach(input => {
+    //                     input.value = '';
+    //                 })
+    //             })
+    //             .catch(error => {
+    //                 btn.textContent = errorText;
+    //             })
+    //     } else {
+    //         btn.textContent = errorText;
+    //         formElements.forEach(input => {
+    //             input.value = '';
+    //         })
+    //     }
+    // }
 
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
+    // const inputError = (inputElem, regular, alert) => {
+    //     for (let i = 0; i < inputElem.length; i++) {
+    //         const textError = document.createElement('span');
 
-            submitForm();
+    //         inputElem[i].parentElement.append(textError);
+    //         textError.classList.add('help-block');
 
-            setTimeout(() => btn.textContent = textOriginal, 2500);
-        })
-    } catch (error) {
-        console.log(error.message);
-    }
+    //         inputElem[i].addEventListener('input', (e) => {
+    //             textError.innerHTML = '';
+    //             e.target.classList.remove('error');
+    //             if (!regular.test(e.target.value)) {
+    //                 e.target.classList.add('error');
+    //                 textError.innerHTML = alert;
+    //             }
+    //         });
+    //     };
+    // }
 
-    inputError(inputName, regularName, 'Вводите пожалуйста буквы');
-    inputError(inputPhone, regularPhone, 'Вводите телефон в формате "+79276802419"');
+    form.addEventListener('click', (e) => {
+        console.log('запуск');
+        e.preventDefault();
+
+        // submitForm();
+
+        // setTimeout(() => btn.textContent = textOriginal, 2500);
+    })
+
+    // try {
+    //     if (!form) {
+    //         throw new Error('Верните форму на место, пожааалуйста))')
+    //     }
+
+    //     form.addEventListener('submit', (e) => {
+    //         console.log('запуск');
+    //         e.preventDefault();
+
+    //         // submitForm();
+
+    //         // setTimeout(() => btn.textContent = textOriginal, 2500);
+    //     })
+    // } catch (error) {
+    //     console.log(error.message);
+    // }
+
+    // inputError(inputName, regularName, 'Вводите пожалуйста буквы');
+    // inputError(inputPhone, regularPhone, 'Вводите телефон в формате "+79276802419"');
 }
 
 export default sendForm
